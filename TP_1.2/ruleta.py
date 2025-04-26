8f46a55bda9adacec619e094e2d3511e263df195
BorsatoMilton <borsatomilton@gmail.com>
2025-04-26 17:31:02 -0300

    feat: añadir variables

--- TP_1.2/ruleta.py
+++ TP_1.2/ruleta.py
@@ -65,23 +65,23 @@ def graficar_corrida(freq_relativa_por_corrida, promedio_por_corrida, varianza_p
 
 def color_num(num):
     if (num in {1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36}):
-        color = "ROJO"
+        color = "rojo"
     elif (num in {2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35}): 
-        color = "NEGRO"
+        color = "negro"
     elif (num == 0):
-        color = "VERDE"
+        color = "verde"
     return color
 
-def fila_num(num):
+def columna_num(num):
     if (num in {1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34}):
-        fila = 1
+        columna = 1
     elif (num in {2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35}):
-        fila = 2
+        columna = 2
     elif (num in {3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36}):
-        fila = 3
+        columna = 3
     else:
-        fila = 0
-    return fila
+        columna = 0
+    return columna
 
 def zona_num(num):
     if (num in range (1,13)):
@@ -101,6 +101,7 @@ def paridad_num(num):
         par = False
     return par
 
+
 def martingala(apuesta_inicial, num_elegido, num, cap):
     if num != num_elegido:
         cap = cap - apuesta_inicial
@@ -110,18 +111,23 @@ def martingala(apuesta_inicial, num_elegido, num, cap):
         apuesta = apuesta_inicial
     return apuesta, cap
 
+    
 #def dalembert():
 
 #def fibonacci():
 
 #----------------------------------- Definicion de argumentos -----------------------------------
 parser = argparse.ArgumentParser(description="Simulación de ruleta")
-
+opciones = parser.add_mutually_exclusive_group(required=True)
 parser.add_argument("-c", type=int, required=True, help="Cantidad de tiradas (entero positivo)")
 parser.add_argument("-n", type=int, required=True, help="Cantidad de corridas (entero positivo)")
-parser.add_argument("-e", type=int, required=True, help="Número elegido (entre 0 y 36)")
+opciones.add_argument("-e", type=int, help="Número elegido (entre 0 y 36)")
 parser.add_argument("-s", type=str, required=True, help="Estrategia elegida (m, f, d, o)")
 parser.add_argument("-a", type=str, required=True, help="Cantidad de capital (i, f)")
+opciones.add_argument("-b", type=str, help="Color (ROJO O NEGRO)")
+opciones.add_argument("-p", type=str, help="Par o Impar")
+opciones.add_argument("-z", type=str, help="1era,2da o 3era Docena")
+opciones.add_argument("-d", type=str, help="1era, 2da o 3era columna")
 
 # ------------------------------------------- Parseo de argumentos -----------------------------------------------------
 args = parser.parse_args()
@@ -142,10 +148,12 @@ if args.c > MAX_TIRADAS:
 if args.n > MAX_CORRIDAS:
     print(f"Error: la cantidad de corridas no debe superar {MAX_CORRIDAS}.")
     sys.exit(1)
-
-if (args.e < 0 or args.e > 36):
-    print("Error: el número elegido (-e) debe estar entre 0 y 36.")
-    sys.exit(1)
+if args.e is not None:
+    if (args.e < 0 or args.e > 36):
+        print("Error: el número elegido (-e) debe estar entre 0 y 36.")
+        sys.exit(1)
+    mundo = args.e
+    eleccion = 1
 
 if (args.s != "m" and args.s != "f" and args.s != "d" and args.s != "o"):
     print("Error: la estrategia elegida (-s) debe ser 'f', 'm', 'd' u 'o'")
@@ -154,19 +162,45 @@ if (args.s != "m" and args.s != "f" and args.s != "d" and args.s != "o"):
 if (args.a != "f" and args.a != "i"):
     print("Error: el capital elegido (-a) debe ser finito (f) o infinito (i)")
     sys.exit(1)
+if args.b is not None:
+    if (args.b != "rojo" and args.b != "negro"):
+        print("Error: el color elegido (-b) debe ser 'rojo' o 'negro'.")
+        sys.exit(1)
+    mundo = args.b
+    eleccion = 2
+    
+if args.p is not None:
+    if (args.p != "par" and args.p != "impar"):
+        print("Error: la paridad elegida (-p) debe ser 'par' o 'impar'.")
+        sys.exit(1)
+    mundo = args.p
+    eleccion = 3
+if args.z is not None:
+    if (args.z !='primera docena' and args.z !='segunda docena' and args.z !='tercera docena'):
+        print("Error: la zona elegida (-z) debe ser 'primera docena', 'segunda docena' o 'tercera docena'.")
+        sys.exit(1)
+    mundo=args.z
+    eleccion = 4
+if args.d is not None:
+    if (args.d !='primera columna' and args.d !='segunda columna' and args.d !='tercera columna'):
+        print("Error: la columna elegida (-d) debe ser 'primera columna', 'segunda columna' o 'tercera columna'.")
+        sys.exit(1)
+    mundo = args.d
+    eleccion = 5
 
-print(f"Tiradas: {args.c}, Corridas: {args.n}, Número elegido: {args.e}, Estrategia elegida: {args.s}, Capital elegido: {args.a}")
+
+print(f"Tiradas: {args.c}, Corridas: {args.n}, Número elegido: {args.e}, Estrategia elegida: {args.s}, Capital elegido: {args.a}, Color elegido: {args.b}, Paridad elegida: {args.p}, Zona elegida: {args.z}, Columna elegida: {args.d} ")
+
 
 num_tiradas = args.c
 num_corridas = args.n
 num_elegido = args.e
 est_elegida = args.s
 cap_elegido = args.a
-#color_deminumero = color_num(args.e)
-#zona_deminumero = zona_num(args.e)
-#fila_deminumero = fila_num(args.e)
-#paridad_deminumero = paridad_num(args.e)
-
+#color_elegido = args.b
+#paridad_elegida=args.p
+#zona_elegida=args.z
+#columna_elegida=args.d
 # --------------------------------------------------------- Inicio de la simulación -------------------------------------------------
 for i in range(num_corridas):
     print("Corrida numero", i + 1)
@@ -183,41 +217,47 @@ for i in range(num_corridas):
     for j in range(num_tiradas):
         if flag == False: 
             valor = random.randint(0, 36)
+            if args.e is not None:
+                mundo_a_comparar = valor
+            if args.b is not None:
+                mundo_a_comparar = color_num(valor)
+            if args.p is not None:
+                mundo_a_comparar = paridad_num(valor)
+            if args.z is not None:
+                mundo_a_comparar = zona_num(valor)
+            if args.d is not None:
+                mundo_a_comparar = columna_num(valor)
             valores_por_corrida.append(valor)
             promedio_por_corrida.append(stats.mean(valores_por_corrida))
-
             if (valor != 0):
-              color_valor = color_num(valor)
-              fila_valor = fila_num(valor)
-              zona_valor = zona_num(valor)
-              par_valor = paridad_num(valor)
-
+              color_aleatorio = color_num(valor)
+              columna_aleatorio = columna_num(valor)
+              zona_aleatorio = zona_num(valor)
+              par_aleatorio = paridad_num(valor)
             if (est_elegida == "m"):
-                ap, capital = martingala(apuesta_inicial, num_elegido, valor, capital_inicial)
+                ap, capital = martingala(apuesta_inicial, mundo, mundo_a_comparar, capital_inicial)
                 print("Capital: ", capital)
                 apuesta_inicial = ap
                 capital_inicial = capital
                 if (capital_inicial <= 0 or capital_inicial < apuesta_inicial):
                     print("banca rota")
                     #tiene que cortar
-            elif (est_elegida == "f"):
-                print("f")
-            elif (est_elegida == "d"):
-                print("d")
-            elif (est_elegida == "o"):
-                print("o")
-
+                elif (est_elegida == "f"):
+                    print("f")
+                elif (est_elegida == "d"):
+                    print("d")
+                elif (est_elegida == "o"):
+                    print("o")
             if j > 0:
                 varianza_por_corrida.append(stats.variance(valores_por_corrida))
                 desvio_por_corrida.append(stats.stdev(valores_por_corrida))
 
-            if (valor == num_elegido):
-                print("El numero elegido", num_elegido,  "tuvo acierto en la tirada numero", j + 1)
-                aciertos_por_corrida += 1
-            freq_relativa_por_corrida.append(aciertos_por_corrida/(j+1))
+            #if (valor == num_elegido):
+            #    print("El numero elegido", num_elegido,  "tuvo acierto en la tirada numero", j + 1)
+            #    aciertos_por_corrida += 1
+            #freq_relativa_por_corrida.append(aciertos_por_corrida/(j+1))
 
     print("Valores: ",valores_por_corrida)
     print("Cantidad de aciertos: ", aciertos_por_corrida)
     graficar_corrida(freq_relativa_por_corrida, promedio_por_corrida, varianza_por_corrida, desvio_por_corrida)
-    print("---------------------------------------------")  
-
+    print("---------------------------------------------")
